dea1de7a60d4e52d458f6622f29bcbb4177fcc8f
vvo806 <vvo22@students.bmchs.org>
2021-12-03 08:08:54 -0600

    added another enemy/usr/bin/python3

--- side_scroller.py
+++ side_scroller.py
@@ -29,7 +29,7 @@ SCORE = 0
 
 #setting up fonts
 font = pygame.font.SysFont("Verdana", 60)
-font_small = pygame.font.SysFont("Verdana", 20)
+font_small = pygame.font.SysFont("Verdana", 30)
 game_over = font.render("Game Over", True, BLACK)
 
 background = pygame.transform.scale(pygame.image.load("forest2.jpg"), (SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -49,7 +49,7 @@ class Enemy(pygame.sprite.Sprite):
       def move(self):
         global SCORE
         self.rect.move_ip(-10,0)
-        if (self.rect.left < 0):
+        if (self.rect.left < 0): #when snake leaves screen on the left
             SCORE += 1
             self.rect.left = 600
             self.rect.center = (600, (random.randint(40, SCREEN_HEIGHT-40)))
@@ -63,19 +63,21 @@ class Player(pygame.sprite.Sprite):
         super().__init__() 
         self.image = pygame.transform.scale(pygame.image.load("mouse.png"), (60,80))
         self.rect = self.image.get_rect()
-        self.rect.center = (160, 520)
+        self.rect.center = (140, 520)
  
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0,5)
+        if self.rect.top > 0:
+            if pressed_keys[K_UP]:
+                self.rect.move_ip(0, -5)
+        if self.rect.bottom < 600:
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0,5)
          
         if self.rect.left > 0:
               if pressed_keys[K_LEFT]:
                   self.rect.move_ip(-5, 0)
-        if self.rect.right < SCREEN_WIDTH:        
+        if self.rect.left > 0:        
               if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(5, 0)
  
@@ -94,7 +96,6 @@ enemies.add(E2)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
-all_sprites.add(E2)
  
 #Adding a new User event 
 INC_SPEED = pygame.USEREVENT + 1
@@ -106,7 +107,7 @@ while True:
     #Cycles through all events occuring  
     for event in pygame.event.get():
         if event.type == INC_SPEED:
-              SPEED += 4
+              SPEED += 5
            
         if event.type == QUIT:
             pygame.quit()
@@ -114,15 +115,24 @@ while True:
  
  
     DISPLAYSURF.blit(background, (0,0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURF.blit(scores, (10,10))
  
     #Moves and Re-draws all Sprites
     for entity in all_sprites:
         DISPLAYSURF.blit(entity.image, entity.rect)
         entity.move()
- 
+
+        if E1.rect.left < 100: #second snake comes in
+            all_sprites.add(E2)
+
+        if SCORE == 20:
+            all_sprites.add(E2)
+
+
     #To be run if collision occurs between Player and Enemy
     if pygame.sprite.spritecollideany(P1, enemies):
-        pygame.mixer.Sound('crash.wav').play()
+        pygame.mixer.Sound('eating_sound.wav').play()
         time.sleep(3)
 
         DISPLAYSURF.fill(RED)
@@ -133,7 +143,7 @@ while True:
             entity.kill() 
         time.sleep(2)
         pygame.quit()
-        sys.exit()        
+        sys.exit()       
          
     pygame.display.update()
     clock.tick(FPS)
